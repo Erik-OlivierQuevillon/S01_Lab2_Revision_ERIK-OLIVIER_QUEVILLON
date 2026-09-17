868a66f8bb0e06e64fdc0d36683c8e6b77a6aef8
Erik-Olivier Quevillon <2075881@cegepmontpetit.ca>
2026-09-17 17:37:58 -0400

    FCT ZombieType Create et Validation
    Ajout de la validation côté client et côté serveur pour ZombieType

--- ZombieParty/ZombieParty/Controllers/ZombieTypeController.cs
+++ ZombieParty/ZombieParty/Controllers/ZombieTypeController.cs
@@ -21,6 +21,17 @@ namespace ZombieParty.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Create(Models.ZombieType zombieType)
+        {
+            if (ModelState.IsValid)
+            {
+                // Ajouter à la BD
+            }
+
+            return this.View(zombieType);
+        }
+
 
     }
 }
--- ZombieParty/ZombieParty/Models/ZombieType.cs
+++ ZombieParty/ZombieParty/Models/ZombieType.cs
@@ -1,8 +1,15 @@
-﻿namespace ZombieParty.Models
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.Contracts;
+
+namespace ZombieParty.Models
 {
     public class ZombieType
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Type Name has to be filled")]
         public string TypeName { get; set; }
     }
 }
